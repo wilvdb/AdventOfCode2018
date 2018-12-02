a80eb9d8b80804e05a32a5b1dddc3b7f5beb55d0
obionewilobi <wilfried.vandenberghe@gmail.com>
2018-12-02 21:57:49 +0100

    Solution for day 2 part 1

---
+++ Day2.py
@@ -0,0 +1,30 @@
+import string
+
+with open("2_input") as f:
+    content = f.readlines()
+
+alphabet = string.ascii_lowercase[:26]
+two = 0
+three = 0
+for line in content:
+    exactlyTwo = False
+    exactlyThree = False
+    print(line)
+    for letter in alphabet:
+        if line.count(letter) == 3:
+            print("--3--", letter)
+            exactlyThree = True
+        if not line.count(letter) == 3 and line.count(letter) == 2:
+            print("--2--", letter)
+            exactlyTwo = True
+
+    if exactlyTwo:
+        two += 1
+        print("Count two ", two)
+    if exactlyThree:
+        three += 1
+        print("Count three ", three)
+
+print(two)
+print(three)
+print(two * three)
